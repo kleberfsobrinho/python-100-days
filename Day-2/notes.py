45480e3f21b2bca53297db40998dbb48f6f13d32
Kleber Sobrinho <kleber.sobrinho@ccc.ufcg.edu.br>
2022-06-08 21:37:59 -0300

    understanding data types

---
+++ Day-2/notes.py
@@ -0,0 +1,9 @@
+# Notes
+## Integer
+
+123_456_789 # O "_" pode ser usado em grandes números para facilitar a leitura do ponto de vista humano.
+
+## f-String
+
+score = 10
+print(f"your score is {score}")
