66e29e9a2bf6df51b9ed1ba0bbc9e5df3b59c8d8
Kleber Sobrinho <kleber.sobrinho@ccc.ufcg.edu.br>
2022-06-09 08:17:02 -0300

    randomisation and python lists

---
+++ Day-4/random-module.py
@@ -0,0 +1,11 @@
+import random
+
+random_integer = random.randint(0, 5)
+print(random_integer)
+
+random_float = random.random()
+print(random_float)
+
+# 0.0000 - 4.99999
+print(5 * random_float)
+
